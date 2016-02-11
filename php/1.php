9a81e2e30b83126d5b591dbb8b25419a8ef7bd2b
Kim.KB <foryou0119@hanmail.net>
2016-02-11 23:52:10 +0900

    My Project1

---
+++ php/1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <input type="button" value="white" onclick="alert('White Background')"/>
+    <input type="button" value="black" onclick="alert('Black Background')"/>
+    <input type="text" id="user_input">
+    <input type="button" value="ID" onclick="alert(document.getElementById('user_input').value)"/>
+  </body>
+</html>
